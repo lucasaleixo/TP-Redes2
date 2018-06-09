3b83744e93233e3ea79de5c469b1be85496371b6
lucasaleixo <lap15.ufpr@gmail.com>
2018-06-09 05:55:23 +0000

    added js

--- page-redes2/main.js
+++ page-redes2/main.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
     $(function(){
         $('.targetDiv').hide();
-         $('.active').click(function(){
+        $('.active').click(function(){
             $('.targetDiv').hide();
+            $("#titulo").show();
+        });
+        
+        $('.showSingle').click(function(){
+              $("#titulo").hide();
+              $('.targetDiv').hide();
+              $('#div'+$(this).attr('target')).show();
         });
 
         $('.showSingle').click(function(){
